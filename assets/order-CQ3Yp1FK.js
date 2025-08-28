4ccbd2571633975a686e1bb23343c728c57ec50e
ziming224 <s11400156316006@gmail.com>
2025-08-28 06:26:59 +0000

    Deploying to gh-pages from @ ziming224/project--vuetify@a01ab3c3fc2f4d4698eb0009c62d3b7a1756d082 🚀

---
+++ assets/order-CQ3Yp1FK.js
@@ -0,0 +1,1 @@
+import{aq as r}from"./index-BseQQyEi.js";const t={create(){return r.apiAuth.post("/order")},getMy(){return r.apiAuth.get("/order/my")},getAll(){return r.apiAuth.get("/order/all")}};export{t as o};
