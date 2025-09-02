700417cf3c71f2b4a0bdaa994d2fa7acecb33a9b
ziming224 <s11400156316006@gmail.com>
2025-09-02 07:15:50 +0000

    Deploying to gh-pages from @ ziming224/project--vuetify@b92d8a03d0215c6b0b25bff3aa8171f2251eccc6 🚀

---
+++ assets/order-B439WUES.js
@@ -0,0 +1,1 @@
+import{ax as r}from"./index-C3zfx7_6.js";const t={create(){return r.apiAuth.post("/order")},getMy(){return r.apiAuth.get("/order/my")},getAll(){return r.apiAuth.get("/order/all")}};export{t as o};
---
+++ assets/product-CVmhSzi5.js
@@ -0,0 +1,1 @@
+import{ax as r}from"./index-C3zfx7_6.js";const u={create(t){return r.apiAuth.post("/product",t)},getAll(){return r.apiAuth.get("/product/all")},get(){return r.api.get("/product")},getId(t){return r.api.get("/product/"+t)},update(t,e){return r.apiAuth.patch(`/product/${t}`,e)}};export{u as p};
