782abba926138945379c4a4fa05e14faaf1b0378
ziming <s11400156316006@gmail.com>
2025-09-03 14:22:38 +0800

    loading畫面

--- src/components/LoadingOverlay.vue
+++ src/components/LoadingOverlay.vue
@@ -1,29 +1,29 @@
 <template>
   <v-overlay
-    :model-value="show"
     class="d-flex align-center justify-center"
+    :model-value="show"
     persistent
     scrim="rgba(255,255,255,1)"
     @update:model-value="$emit('update:show', $event)"
   >
     <DotLottieVue
-      style="height: 600px; width: 600px"
       autoplay
       loop
       :src="animationSrc"
+      style="height: 600px; width: 600px"
     />
   </v-overlay>
 </template>
 
 <script setup>
-import { DotLottieVue } from '@lottiefiles/dotlottie-vue'
-import { defineProps } from 'vue'
+  import { DotLottieVue } from '@lottiefiles/dotlottie-vue'
+  import { defineProps } from 'vue'
 
-const props = defineProps({
-  show: Boolean,
-  animationSrc: {
-    type: String,
-    default: '/animations/SaladCat.lottie'
-  }
-})
+  const props = defineProps({
+    show: Boolean,
+    animationSrc: {
+      type: String,
+      default: '/animations/SaladCat.lottie',
+    },
+  })
 </script>
