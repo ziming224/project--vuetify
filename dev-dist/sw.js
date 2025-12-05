d75f3f87b661a1353ae67f1a4d1421ae92e2330a
zih <s11400156316006@gmail.com>
2025-12-05 17:14:32 +0800

    add

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ums7u9d2g58"
+    "revision": "0.ptoed96ntbo"
   }], {
     "ignoreURLParametersMatching": [/.*/]
   });
