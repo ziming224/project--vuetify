98beac68736d2433430b2d303ea4fa18762a9207
ziming224 <s11400156316006@gmail.com>
2025-12-24 14:03:08 +0000

    Deploying to gh-pages from @ ziming224/project--vuetify@0f0f2ee6ddbaeeab70b740e6bf8fd1bb3c3b7194 🚀

---
+++ assets/order-7f9hFE1_.js
@@ -0,0 +1,1 @@
+import{aD as r}from"./index-BUE9pJ4F.js";const t={create(){return r.apiAuth.post("/order")},getMy(){return r.apiAuth.get("/order/my")},getAll(){return r.apiAuth.get("/order/all")}};export{t as o};
